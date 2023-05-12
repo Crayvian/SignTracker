4de2247d1dfff1e05d78801775d1e8de34a4b178
Crayvian <crayvian@pm.me>
2023-05-12 18:32:18 +0200

    shadow toml jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "de.rubingrube"
@@ -7,12 +8,12 @@ version = "3.0"
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.purpurmc.org/snapshots")
     maven("https://jitpack.io")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("org.purpurmc.purpur", "purpur-api", "1.19.4-R0.1-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7");
     implementation("com.moandjiezana.toml:toml4j:0.7.2");
     compileOnly("org.bstats:bstats-bukkit:2.2.1");
